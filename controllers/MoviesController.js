16d1ee3cd0bbd30c7da75808194e983979e54793
deca-francesco <francescodecarlo70@gmail.com>
2024-12-10 19:38:17 +0100

    ADD: ER diagram

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -39,25 +39,28 @@ function show(req, res) {
     `
     // execute the sql query for movie
     connection.query(movieSql, [Number(id)], (err, results) => {
+        // handle errors
         if (err) return res.status(500).json({ error: err })
-
+        // save results in a constant
         const movie = results[0]
-
+        // handle the not found error
         if (!movie) {
             return res.status(404).json({
                 error: `404! Nessun risultato`
             })
         }
-
+        // execute query for reviews
         connection.query(reviewsSql, [Number(id)], (err, reviewsResults) => {
+            // handle errors
             if (err) return res.status(500).json({ error: err })
-
+            // save results as a property of movie
             movie.reviews = reviewsResults
-
+            // create the response
             const responseData = {
                 data: movie
             }
             console.log(responseData);
+            // return the response
             res.status(200).json(responseData)
         })
     })
